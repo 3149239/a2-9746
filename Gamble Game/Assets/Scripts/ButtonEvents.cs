9a2e79c2071ac6a66cba9ce155418f6b55f1e9f1
3149239 <jamie-stuff@hotmail.com>
2017-05-08 13:18:15 +1000

    First upload
    v0.01

---
+++ Gamble Game/Assets/Scripts/ButtonEvents.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonEvents : MonoBehaviour
+{
+
+    public GameObject whoToWinPanel;    //First panel
+    public GameObject betSizePanel;     //Second Panel
+    public GameObject diceRollPanel;    //Third Panel
+    public GameObject healthPanel;      //Forth Panel
+
+    public bool redChosen;              //red or blue chosen
+    public bool coins10chosen;          //10 coins bet or 25
+    public bool betOdd;                 //bet odd or even
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void ChangeToBetSizeRed()       //Sets gameobjects to active or hidden
+    {
+        whoToWinPanel.SetActive(false);
+        betSizePanel.SetActive(true);
+        redChosen = true;
+    }
+
+    public void ChangeToBetSizeBlue()       //Sets gameobjects to active or hidden
+    {
+        whoToWinPanel.SetActive(false);
+        betSizePanel.SetActive(true);
+        redChosen = false;
+    }
+
+    public void ChangeToDiceRoll10()      //Sets gameobjects to active or hidden
+    {
+        betSizePanel.SetActive(false);
+        diceRollPanel.SetActive(true);
+        coins10chosen = true;
+    }
+    public void ChangeToDiceRoll25()      //Sets gameobjects to active or hidden
+    {
+        betSizePanel.SetActive(false);
+        diceRollPanel.SetActive(true);
+        coins10chosen = false;
+    }
+    public void ChangeToHealthOdd()      //Sets gameobjects to active or hidden
+    {
+        diceRollPanel.SetActive(false);
+        healthPanel.SetActive(true);
+        betOdd = true;
+    }
+    public void ChangeToHealthEven()      //Sets gameobjects to active or hidden
+    {
+        diceRollPanel.SetActive(false);
+        healthPanel.SetActive(true);
+        betOdd = false;
+    }
+}
