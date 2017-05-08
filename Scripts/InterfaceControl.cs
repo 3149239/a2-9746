08fe6ae3c86c2a7b1cf4f483607207cadd0b289e
3149239 <jamie-stuff@hotmail.com>
2017-05-08 13:23:17 +1000

    Scripts upload
    v0.01

---
+++ Scripts/InterfaceControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InterfaceControl : MonoBehaviour {
+
+    public interface IMyDiceInterface
+    {
+        void MyDiceRoll();
+    }
+
+    class MyDiceInterface : IMyDiceInterface
+
+    {
+
+        public void MyDiceRoll()
+
+        {
+
+            if (GameObject.Find("Scripts").GetComponent<CharacerAttacks>().oddRoll == true)
+            {
+                Debug.Log("The dice rolled was ODD");
+            }
+
+            if (GameObject.Find("Scripts").GetComponent<CharacerAttacks>().oddRoll == false)
+            {
+                Debug.Log("The dice rolled was EVEN");
+            }
+        }
+
+    }
+
+}
