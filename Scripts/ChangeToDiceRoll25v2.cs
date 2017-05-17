3b83a73a8028855e4f636741f14a4f8a0ab63225
3149239 <jamie-stuff@hotmail.com>
2017-05-17 17:28:16 +1000

    v0.12
    Added polymorphism

--- Scripts/ChangeToDiceRoll25v2.cs
+++ Scripts/ChangeToDiceRoll25v2.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class ChangeToDiceRoll25v2 : ButtonEventsInh
 
 {
+
+    public TakeCoins myTakeCoins;   //assigns take my coins to mytakecoins
+
     public override void CloseAllPanels()
     {
         base.CloseAllPanels();                      //closes all panels
         diceRollPanel.SetActive(true);              //sets dice roll panel to active
-        myCharacterAttacks.bet10coins = false;       //says the player chose to bet 25 coins
-        myCharacterAttacks.playerCoins -= 25;        //takes 25 coins from the player
+        myTakeCoins = GameObject.Find("Scripts").GetComponent<TakeCoins>();                         //attaches script to Take Coins polymorphism script
+        myCharacterAttacks.playerCoins = myTakeCoins.takeCoins(myCharacterAttacks.playerCoins, 25); //takes 25 coins away
+
     }
 }
---
+++ Scripts/TakeCoins.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeCoins : MonoBehaviour
+{
+
+    public float takeCoins(float a, float b)    //take coins through polymorphism
+    {
+        return a - b;
+    }
+}
